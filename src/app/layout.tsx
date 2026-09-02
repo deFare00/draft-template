1188c29567761e2fd3aebb77e94f3600eb49be99
defarhan <defarhannugraha1@gmail.com>
2026-09-02 13:55:04 +0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
       <body>{children}</body>
     </html>
